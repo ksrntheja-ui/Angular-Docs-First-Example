5f44e6670fbebd904fdb4a98e41ff1e3673f96c3
KSRNTheja <srntkolla@gmail.com>
2020-01-27 10:23:08 +0530

    05-Created new 'top-bar'component : ng generate component top-bar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
+
 import {AppComponent} from './app.component';
+import {TopBarComponent} from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent // ng generate component top-bar
   ],
   imports: [
     BrowserModule,
