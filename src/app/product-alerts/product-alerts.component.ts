5736c9057adb8c5fe4c07fbb5e74f9ab164fa7e5
KSRNTheja <srntkolla@gmail.com>
2020-01-28 21:28:59 +0530

    22-Added @Input() [indicates that the property value passes in from the component's parent] to product-alert component.

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -6,6 +6,21 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
+
+
+  /*
+  In the ProductAlertsComponent class definition, define a
+    property named product with an @Input() decorator.
+
+  The @Input() decorator indicates that the
+    property value
+    passes in from the component's' +
+    'parent,' +
+    'the product list component.
+   */
+
+  @Input
+  product;
 
   constructor() {
   }
