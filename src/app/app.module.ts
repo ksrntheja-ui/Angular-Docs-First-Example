d9a057d4eb5dc3dac3415e7fdb766bbccbe15d38
KSRNTheja <srntkolla@gmail.com>
2020-01-28 21:19:29 +0530

    21-Created new 'product-alerts'component : ng generate component product-alerts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {TopBarComponent} from './top-bar/top-bar.component';
 import {ProductListComponent} from './product-list/product-list.component';
+import {ProductAlertsComponent} from './product-alerts/product-alerts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent, // ng generate component top-bar
-    ProductListComponent // ng generate component product-list
+    ProductListComponent, // ng generate component product-list
+    ProductAlertsComponent // ng generate component product-alerts
   ],
   imports: [
     BrowserModule,
