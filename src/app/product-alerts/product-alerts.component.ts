fc73e0b75c393a4e0e12da9187a7a8912e4f563b
KSRNTheja <srntkolla@gmail.com>
2020-01-28 21:53:11 +0530

    26-Allowed the product-alert component to emit an event when the value of the notify property changes.

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -27,6 +27,40 @@ export class ProductAlertsComponent implements OnInit {
 
   @Input()
   product;
+
+  /*
+  To make the "Notify Me" button work, you need to configure two things:
+
+  -> the product
+    alert
+    component to
+    emit an event
+    when the user
+    clicks "Notify Me".
+  -> the product
+    list
+    component to
+    act on
+    that event
+   */
+
+  /*
+  In the component class, define a
+    property named
+    notify with an
+    @Output() decorator and an
+    instance of EventEmitter().
+
+  This allows the product
+    alert
+    component to
+    emit an
+    event when the
+    value of the notify property changes.
+  */
+
+  @Output()
+  notify = new EventEmitter();
 
   constructor() {
   }
