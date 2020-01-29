f11d5b86f1deab2fa450eb026f9ecac867636986
KSRNTheja <srntkolla@gmail.com>
2020-01-29 10:45:05 +0530

    35-Defined the product property.

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -7,6 +7,9 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
+  // Define the product property
+  product;
+
   constructor() {
   }
 
