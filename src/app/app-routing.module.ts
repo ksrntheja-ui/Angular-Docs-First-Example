3e6554ffd6e73df559d4841d5e40c05b02287872
KSRNTheja <srntkolla@gmail.com>
2020-01-29 10:09:22 +0530

    32-Added a route for product-details, hit products/:productId to check.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
 import {ProductListComponent} from './product-list/product-list.component';
+import {ProductDetailsComponent} from './product-details/product-details.component';
 
 const routes: Routes = [{
   path: '', component: ProductListComponent
-}];
+}, {
+  /*Add a route for product-details, with a
+  path of products/:productId and
+  ProductDetailsComponent for the component.*/
+  path: 'products/:productId', component: ProductDetailsComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
