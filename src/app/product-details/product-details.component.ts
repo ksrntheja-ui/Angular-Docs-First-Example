01bc6521445e2390bf1a421f9e873e573bb44e34
KSRNTheja <srntkolla@gmail.com>
2020-01-30 10:31:51 +0530

    39-Uderstanding .subscribe and ParamMap.

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -27,4 +27,91 @@ export class ProductDetailsComponent implements OnInit {
     });
   }
 
+  /*
+  ngOnInit() {
+    console.log(this.activatedRoute.params);
+    console.log(this.activatedRoute.paramMap);
+    // console.log(this.activatedRoute.paramMap.get('productId'));
+    // Property 'get' does not exist on type 'Observable<ParamMap>'.
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      console.log(paramMap);
+      this.product = products[paramMap.get('productId')];
+    });
+  }
+   */
+
+
+  // Remove braces around arrow function body
+  /*
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap => this.product = products[paramMap.get('productId')]);
+  }
+  */
+
+
+  // Specify type explicitly
+  /*
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      this.product = products[paramMap.get('productId')];
+    });
+  }
+  */
+
+
+  // Propagate to destructuring declaration
+  /*
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(({get}) => {
+      this.product = products[get('productId')];
+    });
+  }
+   */
+
+  // Introduce local variable - let
+  // TSLint: Identifier 'next' is never reassigned; use 'const' instead of 'let'.(prefer-const)
+  /*
+  ngOnInit() {
+    let next = params => {
+      this.product = products[+params.get('productId')];
+    };
+    this.activatedRoute.paramMap.subscribe(next);
+  }
+   */
+
+
+  // Introduce local variable - var
+  // TSLint: Forbidden 'var' keyword, use 'let' or 'const' instead(no-var-keyword)
+  // TSLint: Identifier 'next' is never reassigned; use 'const' instead of 'var'.(prefer-const)
+  /*
+  ngOnInit() {
+    var next = params => {
+      this.product = products[+params.get('productId')];
+    };
+    this.activatedRoute.paramMap.subscribe(next);
+  }
+   */
+
+  // Introduce local variable - const
+  /*
+  ngOnInit() {
+    const next = params => {
+      this.product = products[+params.get('productId')];
+    };
+    this.activatedRoute.paramMap.subscribe(next);
+  }
+   */
+
+
+  // convert to a named function
+  /*
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(getProduct);
+
+    function getProduct(params) {
+      this.product = products[+params.get('productId')];
+    }
+  }
+   */
+
 }
