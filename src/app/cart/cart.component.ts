c649053bf2037180556e402eb8b29a26c79687b5
KSRNTheja <srntkolla@gmail.com>
2020-01-30 21:51:03 +0530

    55-Imported the CartService from the cart.service.ts file to cart-component.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 
+import {CartService} from '../services/cart.service';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
