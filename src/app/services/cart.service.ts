0994510f32a70c1b1edd28bb651c5c8b6105f4c1
KSRNTheja <srntkolla@gmail.com>
2020-01-30 21:13:39 +0530

    43-Defined an items property to store the list (array) of the current products in the cart.

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  constructor() { }
+  // Define an items property to store the list (array) of the current products in the cart.
+  items = [];
+
+  constructor() {
+  }
 }
