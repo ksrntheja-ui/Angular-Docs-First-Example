6d4152a213777047617f08fbb76bf710668f37e6
KSRNTheja <srntkolla@gmail.com>
2020-01-30 21:46:24 +0530

    54-Added routing (a URL pattern) for the cart-component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {Routes, RouterModule} from '@angular/router';
 
 import {ProductListComponent} from './product-list/product-list.component';
 import {ProductDetailsComponent} from './product-details/product-details.component';
+import {CartComponent} from './cart/cart.component';
 
 const routes: Routes = [{
   path: '', component: ProductListComponent
@@ -11,6 +12,9 @@ const routes: Routes = [{
   path of products/:productId and
   ProductDetailsComponent for the component.*/
   path: 'products/:productId', component: ProductDetailsComponent
+}, {
+  // Add routing (a URL pattern) for the cart component
+  path: 'cart', component: CartComponent
 }
 ];
 
