696ed1a08482948fb587a3ea5be19724ef0239a9
KSRNTheja <srntkolla@gmail.com>
2020-01-31 22:11:06 +0530

    67-Inject cart service to Shipping-Component.

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 
+import {CartService} from '../services/cart.service';
+
 @Component({
   selector: 'app-shipping',
   templateUrl: './shipping.component.html',
@@ -7,7 +9,10 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ShippingComponent implements OnInit {
 
-  constructor() {
+  constructor(
+    // Inject cart service
+    private cartService: CartService
+  ) {
   }
 
   ngOnInit() {
