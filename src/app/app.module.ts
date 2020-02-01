b630830e870c8f80531101199b5511cc7ef696b1
KSRNTheja <srntkolla@gmail.com>
2020-02-01 16:15:05 +0530

    76-Import ReactiveFormsModule in app-module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import {ProductDetailsComponent} from './product-details/product-details.compone
 import {CartComponent} from './cart/cart.component';
 import {HttpClientModule} from '@angular/common/http';
 import {ShippingComponent} from './shipping/shipping.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import {ShippingComponent} from './shipping/shipping.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
